790b627026b3a236dced32b9a5c118240a204c53
Lance Gliser <lance.gliser@torch.ai>
2022-03-23 16:49:56 -0400

    Bug/collections endpoint commas (#23)
    * Separated IAutocompleteParams["collection_ids"] out to type AutocompleteCollectionId and changed it to an optional string
    * exported IAutocompleteEntity
    * Modified autocomplete.search's collection_ids property to use an array of AutocompleteCollectionId and concat internally.
    * Added a test for "should filter by collection sub ids"

--- src/components/autocomplete/Autocomplete.types.ts
+++ src/components/autocomplete/Autocomplete.types.ts
@@ -1,29 +1,31 @@
 import { IEntityIdentifier } from "../components.types";
 
+export type AutocompleteCollectionId =
+  | "organizations"
+  | "organization.companies"
+  | "organization.investors"
+  | "organization.schools"
+  | "people"
+  | "person.investors"
+  | "funding_rounds"
+  | "acquisitions"
+  | "investments"
+  | "events"
+  | "press_references"
+  | "funds"
+  | "event_appearances"
+  | "ipos"
+  | "ownerships"
+  | "categories"
+  | "category_groups"
+  | "locations"
+  | "jobs"
+  | "principals";
+
 export interface IAutocompleteParams {
   query: string;
-  collection_ids?: (
-    | "organizations"
-    | "organization.companies"
-    | "organization.investors"
-    | "organization.schools"
-    | "people"
-    | "person.investors"
-    | "funding_rounds"
-    | "acquisitions"
-    | "investments"
-    | "events"
-    | "press_references"
-    | "funds"
-    | "event_appearances"
-    | "ipos"
-    | "ownerships"
-    | "categories"
-    | "category_groups"
-    | "locations"
-    | "jobs"
-    | "principals"
-  )[];
+  /** One or more AutocompleteCollectionIds separated by "," */
+  collection_ids?: string;
   limit?: number;
 }
 
@@ -31,7 +33,7 @@ export interface IAutocompleteResult {
   entities: IAutocompleteEntity[];
 }
 
-interface IAutocompleteEntity {
+export interface IAutocompleteEntity {
   identifier: IEntityIdentifier;
   facet_ids: string[];
   short_description: string;
